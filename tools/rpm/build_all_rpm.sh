1ff35f5b681d6f4f50754294362a5258ef48e702
Mictronics <github@mictronics.de>
2024-09-17 16:28:49 +0200

    Added rpms subfolder.

--- tools/rpm/build_all_rpm.sh
+++ tools/rpm/build_all_rpm.sh
@@ -4,6 +4,8 @@ set -ex
 
 TULEAP_SOURCES="$1"
 RPM_BUILD="$2"
+TULEAP_OUT_DIR=/var/rpms/tuleap-$(cat "$SRC_DIR/VERSION")
+mkdir -p $TULEAP_OUT_DIR
 
 push_nix_build_cache=''
 if [ -n "${CACHIX_AUTH_TOKEN}" ]; then
@@ -26,16 +28,16 @@ nix-build "$TULEAP_SOURCES"/tools/rpm/tuleap-rpms.nix \
     --arg tuleapSourceTarballPath "$(readlink "$TULEAP_SOURCES"/result-tarball)" \
     --out-link "$build_tmp/packages"
 
-cp -v -L "$build_tmp/packages/"*.rpm "/var/rpms/"
+cp -v -L "$build_tmp/packages/"*.rpm $TULEAP_OUT_DIR
 cp -v -L "$build_tmp/packages/"*.rpm "$RPM_BUILD/RPMS/noarch/"
 
 ${push_nix_build_cache} "$TULEAP_SOURCES/"tools/rpm/build_nix_derivation.sh "$TULEAP_SOURCES/"tools/rpm/rpm-additional-packages.nix "$build_tmp/packages"
 
-cp -v -L "$build_tmp/packages/"*.rpm "/var/rpms/"
+cp -v -L "$build_tmp/packages/"*.rpm $TULEAP_OUT_DIR
 cp -v -L "$build_tmp/packages/"*.rpm "$RPM_BUILD/RPMS/noarch/"
 if [ -d "$build_tmp/packages/${OS}" ]
 then
-    cp -v -L "$build_tmp/packages/${OS}"/*.rpm "/var/rpms/"
+    cp -v -L "$build_tmp/packages/${OS}"/*.rpm $TULEAP_OUT_DIR
     cp -v -L "$build_tmp/packages/${OS}"/*.rpm "$RPM_BUILD/RPMS/noarch/"
 fi
 chmod -v --recursive u+w "$RPM_BUILD/RPMS/noarch/"
